46b15f9fef12b87331a8a79861f441f159b8f94f
Stanly Shianovskiy <st-small@rambler.ru>
2023-09-25 19:40:54 +0300

    Initial Commit

---
+++ GoldenSewingSUI/GoldenSewingSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoldenSewingSUIApp.swift
+//  GoldenSewingSUI
+//
+//  Created by Stanly Shiyanovskiy on 25.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct GoldenSewingSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
